af64ebedd08bfc1a88e691bac532973b71b1e197
李梦平 <jaker0796@163.com>
2017-04-29 22:39:58 +0800

    初次创建项目

---
+++ application/route.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+return [
+    '/' => '/index/index',
+    '/place' => '/index/place/index',
+    '/place/intro' => '/index/place/intro',
+    '/place/history' => '/index/place/history',
+    '/place/area' => '/index/place/area',
+    '/place/scenic' => '/index/place/scenic',
+    '/place/food' => '/index/place/food',
+    '/place/celebrity' => '/index/place/celebrity',
+    '/place/school' => '/index/place/school',
+//    '__pattern__' => [
+//        'name' => '\w+',
+//    ],
+//    '[hello]'     => [
+//        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+//        ':name' => ['index/hello', ['method' => 'post']],
+//    ],
+
+];
