308a658046dd076918f48c42fcc1c48606bd2571
李梦平 <jaker0796@163.com>
2017-05-10 21:37:39 +0800

    thinkphp目录 单元测试目录

---
+++ tests/StackTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/7
+ * Time: 0:55
+ */
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+
+    }
+}
---
+++ tests/env.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/7
+ * Time: 1:04
+ */
+require __DIR__ . '/../vendor/autoload.php';
