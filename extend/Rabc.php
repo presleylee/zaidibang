db86f859b51a61be5ef559e46f9aac72aa229aaf
李梦平 <jaker0796@163.com>
2018-04-16 20:16:22 +0800

    样式更 新

---
+++ extend/Rabc.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/13
+ * Time: 21:55
+ */
+
+namespace Rabc;
+
+use think\Db;
+use App\Common\Model\Admin;
+use App\Common\Model\AdminNode;
+use App\Common\Model\AdminRole;
+use App\Common\Model\AdminAccess;
+
+class Rabc
+{
+
+}
